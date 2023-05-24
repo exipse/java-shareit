8a7227237e98294163a1ab737dca700a35357d51
Ivanov Pavel <exipse@yandex.ru>
2023-05-24 23:48:14 +0300

    transact

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.service;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.DBConstraintException;
 import ru.practicum.shareit.exception.UserNoFoundException;
